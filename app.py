0e851750364bb6fb0e94d57b74d17279d4b45386
Shashank Nidhi <shashanknidhi617@gmail.com>
2023-05-07 11:29:37 +0530

    updated plot for app.py

--- app.py
+++ app.py
@@ -14,11 +14,16 @@ def create_plot(txtpath):
     benignSample = np.log(scores[FMgrace+ADgrace+1:10000])
     logProbs = norm.logsf(np.log(scores), np.mean(benignSample), np.std(benignSample))
 
-    fig,ax = plt.subplots()
-    ax.scatter(range(FMgrace+ADgrace+1,len(scores)),scores[FMgrace+ADgrace+1:],s=0.6,c=logProbs[FMgrace+ADgrace+1:],cmap='RdYlGn')
-    ax.set_xlabel('Packet Number')
-    ax.set_ylabel('RMSE')
-    ax.set_title('RMSE Anomaly Scores')
+    plt.figure(figsize=(10,5))
+    fig = plt.scatter(range(FMgrace+ADgrace+1,len(scores)),scores[FMgrace+ADgrace+1:],s=0.6,c=logProbs[FMgrace+ADgrace+1:],cmap='RdYlGn')
+    plt.yscale("log")
+    plt.title("Anomaly Scores from Kitsune's Execution Phase")
+    plt.ylabel("RMSE (log scaled)")
+    plt.xlabel("Time elapsed [min]")
+    figbar=plt.colorbar()       
+    figbar.ax.set_ylabel('Log Probability\n ', rotation=270)
+    print(type(fig))
+    print(fig)
     return fig
 for path in os.listdir('anomaly_scores'):
     st.pyplot(create_plot('anomaly_scores'+path))
--- test.py
+++ test.py
@@ -8,7 +8,46 @@
 # for i in range(1,5):
 #     file = "captures/"+str(i)+".pcapng"
 #     output = open(file, "x")
+# import os
+# print(os.listdir("D:/tech-a-thon/tech-a-thon/anomaly_scores"))
+# os.remove("D:/tech-a-thon/tech-a-thon/anomaly_scores/"+os.listdir("D:/tech-a-thon/tech-a-thon/anomaly_scores")[-1])
+# print(os.listdir("D:/tech-a-thon/tech-a-thon/anomaly_scores"))
+
+# import subprocess
+import sys
 import os
-print(os.listdir("D:/tech-a-thon/tech-a-thon/anomaly_scores"))
-os.remove("D:/tech-a-thon/tech-a-thon/anomaly_scores/"+os.listdir("D:/tech-a-thon/tech-a-thon/anomaly_scores")[-1])
-print(os.listdir("D:/tech-a-thon/tech-a-thon/anomaly_scores"))
+
+# # check if the script is running with administrative privileges
+if os.getuid():
+    # if not, re-run the script with elevated privileges
+    args = ['powershell', '-command', f'Start-Process "{sys.executable}" -ArgumentList "{sys.argv[0]}" -Verb runAs']
+    subprocess.run(args)
+    sys.exit()
+
+# # specify the IP addresses to block
+# malicious_ips = ['10.0.0.100']
+
+# # execute the netsh command to add firewall rules for each IP address
+# for ip in malicious_ips:
+#     # block incoming traffic from the IP address
+#     command = f"netsh advfirewall firewall add rule name='Block Incoming from {ip}' dir=in interface=any action=block remoteip={ip}"
+#     subprocess.call(command, shell=True)
+
+#     # block outgoing traffic to the IP address
+#     command = f"netsh advfirewall firewall add rule name='Block Outgoing to {ip}' dir=out interface=any action=block remoteip={ip}"
+#     subprocess.call(command, shell=True)
+
+#     # print the status of the firewall rules
+#     command = f"netsh advfirewall firewall show rule name='Block Incoming from {ip}'"
+#     output = subprocess.check_output(command, shell=True)
+#     print(output.decode())
+
+#     command = f"netsh advfirewall firewall show rule name='Block Outgoing to {ip}'"
+#     output = subprocess.check_output(command, shell=True)
+#     print(output.decode())
+
+import subprocess
+
+ip_address = '10.0.0.100'
+
+subprocess.run(['netsh', 'advfirewall', 'firewall', 'add', 'rule', 'name=BlockIP', 'dir=in', 'action=block', f'remoteip={ip_address}'])
