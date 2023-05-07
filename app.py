a2bad0ab3218485e4c930943d6e6e99f6c101fef
Shashank Nidhi <shashanknidhi617@gmail.com>
2023-05-07 12:42:08 +0530

    image display streamlit

--- app.py
+++ app.py
@@ -1,29 +1,55 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import norm
 import os
-def create_plot(txtpath):
-    FMgrace = 1000 
-    ADgrace = 1000
-    with open(txtpath,'r') as file:
-        scores = file.readlines()
-    scores = [float(score) for score in scores]
-    
-    benignSample = np.log(scores[FMgrace+ADgrace+1:10000])
-    logProbs = norm.logsf(np.log(scores), np.mean(benignSample), np.std(benignSample))
+from PIL import Image
+st.write(""" # Dashboard 
+Displays Threats 
+""")
 
-    plt.figure(figsize=(10,5))
-    fig = plt.scatter(range(FMgrace+ADgrace+1,len(scores)),scores[FMgrace+ADgrace+1:],s=0.6,c=logProbs[FMgrace+ADgrace+1:],cmap='RdYlGn')
-    plt.yscale("log")
-    plt.title("Anomaly Scores from Kitsune's Execution Phase")
-    plt.ylabel("RMSE (log scaled)")
-    plt.xlabel("Time elapsed [min]")
-    figbar=plt.colorbar()       
-    figbar.ax.set_ylabel('Log Probability\n ', rotation=270)
-    return fig
-for path in os.listdir('anomaly_scores'):
-    st.pyplot(create_plot('anomaly_scores'+path))
-    
-             
+
+
+def display_images(images):
+    # st.write(
+    #     """
+    #     <style>
+    #     .row-container {
+    #         display: flex;
+    #         flex-wrap: wrap;
+    #         justify-content: center;
+    #         margin-bottom: 20px;
+    #     }
+    #     .column-container {
+    #         flex-basis: 30%;
+    #         margin-right: 10px;
+    #         margin-bottom: 10px;
+    #     }
+    #     .column-container:last-child {
+    #         margin-right: 0;
+    #     }
+    #     .column-container img {
+    #         width: 100%;
+    #         height: auto;
+    #     }
+    #     </style>
+    #     """
+    # )
+
+    # st.write('<div class="row-container">')
+
+    for image in images:
+        # st.write('<div class="column-container">')
+        st.image(image, use_column_width="auto")
+        # st.write('</div>')
+
+    # st.write('</div>')
+
+# Specify the folder path containing the images
+folder_path = "output"
+
+# Get the list of image files in the folder
+image_files = [f for f in os.listdir(folder_path) if f.endswith((".jpg", ".jpeg", ".png"))]
+
+# Read and store the images
+images = [Image.open(os.path.join(folder_path, image_file)) for image_file in image_files]
+
+# Display the images
+display_images(images)
