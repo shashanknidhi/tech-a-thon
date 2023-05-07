0a22c9ccfa03d3514f40af8a865b82cfd66a5165
Shashank Nidhi <shashanknidhi617@gmail.com>
2023-05-07 08:10:43 +0530

    streamlit app

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-import pyshark
-import pandas as pd
 import streamlit as st
-import plotly.express as px
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats import norm
+import os
+def create_plot(txtpath):
+    FMgrace = 1000 
+    ADgrace = 1000
+    with open(txtpath,'r') as file:
+        scores = file.readlines()
+    scores = [float(score) for score in scores]
+    
+    benignSample = np.log(scores[FMgrace+ADgrace+1:10000])
+    logProbs = norm.logsf(np.log(scores), np.mean(benignSample), np.std(benignSample))
 
-# Load Wireshark capture file into a Pandas DataFrame
-cap = pyshark.FileCapture('my_capture.pcap')
-df = pd.DataFrame([{'src': pkt.ip.src, 'dst': pkt.ip.dst, 'protocol': pkt.transport_layer} for pkt in cap])
-
-# Preprocess and clean the data
-df = df.drop_duplicates()
-df['protocol'] = df['protocol'].apply(lambda x: x.upper())
-
-# Define the layout of the dashboard
-st.title('Wireshark Capture Dashboard')
-protocols = df['protocol'].unique()
-selected_protocol = st.sidebar.selectbox('Select protocol', protocols)
-
-# Write the code to generate the visualizations
-filtered_df = df[df['protocol'] == selected_protocol]
-counts = filtered_df['src'].value_counts()
-fig = px.bar(counts, x=counts.index, y=counts.values)
-
-# Add interactivity to the dashboard
-st.plotly_chart(fig)
+    fig,ax = plt.subplots()
+    ax.scatter(range(FMgrace+ADgrace+1,len(scores)),scores[FMgrace+ADgrace+1:],s=0.6,c=logProbs[FMgrace+ADgrace+1:],cmap='RdYlGn')
+    ax.set_xlabel('Packet Number')
+    ax.set_ylabel('RMSE')
+    ax.set_title('RMSE Anomaly Scores')
+    return fig
+for path in os.listdir('anomaly_scores'):
+    st.pyplot(create_plot('anomaly_scores'+path))
+    
+             
